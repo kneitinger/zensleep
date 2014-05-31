634a5d2b57b5f1f2e952340b45b3bf9e983e9fbe
Kyle Kneitinger <kylejkneitinger@gmail.com>
2014-05-30 19:57:13 -0700

    Added command line arguments with getopts

--- zensleep.sh
+++ zensleep.sh
@@ -1,23 +1,53 @@
 #!/bin/sh
-# ZenSleep
-# Checks if battery level is equal to 1st argument
-#   if so, start a 20 second cancellable counter to suspend
+# zensleep
+#   A lightweight gtk sleep notifier.
+#     When called with no arguments, zensleep begins a 20-second countdown
+#     before running pm-suspend.  The time of the countdown (in seconds) can be
+#     overriden with the -t flag, the sleep command can be overriden with the
+#     -s flag.  If the -p flag is used, the script only executes if the level of
+#     the battery's charge matches the value provided
 
-if acpi -b | grep "Discharging, $1%"; then
-    (
-    for i in {0..20}
-    do
-        let t=20-$i
-        let i*=5
-        echo "$i"
-        echo "# System will suspend in $t seconds..."
-        sleep 1
-    done
-    ) |
-    zenity --progress \
-        --title="Low Battery" \
-        --auto-close
-    if [ "$?" = 0 ] ; then
-        sudo pm-suspend
+# Default values
+sleepCommand=pm-suspend
+sleepTime=20
+
+while getopts :p:s:t: option
+do
+    case "$option" in
+    p)
+         batPercent=$OPTARG
+         ;;
+    s)
+         sleepCommand=$OPTARG
+         ;;
+    t)
+         sleepTime=$OPTARG
+         ;;
+    [?])
+        echo "Usage: $0 [-p battery-percentage] [-s sleep-command] [-t countdown-time]"
+		exit 1
+        ;;
+    esac
+done
+
+# If a battery percentage value is provided using the -p flag
+# and it doesn't match the current battery level, exit the script
+if [[ -n $batPercent ]]; then
+    if [[ -z `acpi -b | grep "Discharging, $batPercent%"` ]]; then
+       exit 1
     fi
+fi
+
+(
+for (( c=0; c<=$sleepTime; c++ ))
+do
+
+    echo "$((c*100/sleepTime))"
+    echo "# System will suspend in $((sleepTime-c)) seconds..."
+    sleep 1
+done
+) |
+zenity --progress --title="Low Battery" --auto-close
+if [ "$?" = 0 ] ; then
+    $sleepCommand
 fi
